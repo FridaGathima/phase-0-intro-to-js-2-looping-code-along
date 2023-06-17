7ee9a79a2e7f5117c1b231e9198dc472e0514eab
+ <+>
2023-06-17 13:53:30 +0300

    Completed assignment 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+
+const cards = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards() {
+    const copyOfCards = [`Thank you, ${cards}, for the wonderful suprise gift!`]
+    for (let i=0; i< cards.length; i++){
+    console.log (`Thank you, ${cards[i]}, for the wonderful suprise gift!`)
+    debugger;
+    }
+    return copyOfCards;
+} 
+
+writeCards()
